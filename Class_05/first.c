2329acf362baef963c242913b8f739f9a3217c26
Aleksejs63 <aleksejs.jakimovics@edu.rtu.lv>
2023-09-26 10:37:02 +0300

    class_05

---
+++ Class_05/first.c
@@ -0,0 +1,13 @@
+// vienas rindas komentārs
+
+/* komentāra bloka sākums
+...
+...
+komentāra bloka beigas*/
+
+int main()
+ {
+
+ return 0;
+
+ }
---
+++ Class_05/second.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// vienas rindas komentārs
+
+/* komentāra bloka sākums
+...
+...
+komentāra bloka beigas*/
+
+int main()
+ {
+
+ printf("Hello World\n"); // \n - darbības simbols - kursora pārcelšana jaunajā rindā.
+                          // analoģija ar echo -e "Hello World\n"
+ printf("\a");             //  \a - skaņas signāls
+ return 0;
+ }

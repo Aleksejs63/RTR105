bf51d890167826a559c78f0f5ff99fb97d815dc0
Aleksejs63 <aleksejs.jakimovics@gmail.com>
2023-11-14 19:34:22 +0200

    md

---
+++ Majasdarbi/md3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int first, second;
+    int i;
+    int seciba;
+    char burti[3];
+    printf("Ludzu ievadiet tris burtus: \n");
+    
+
+    for (i = 0; i < 3; i++)
+      scanf("%c", &burti[i]);
+    
+    for (i = 0; i < 3; i++)
+    {
+
+
+    }
+
+    // printf("Izvelaties rezimu. Alfabeta seciba - ievadiet 1, preteji ievadiet 0: \n");
+    // scanf("%d", &seciba);
+
+    // if (seciba = 1)
+    // {
+        
+        
+    // }
+
+
+    return 0;
+
+}
---
+++ Majasdarbi/md4.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    int faktorials;
+    printf("Izvelaties datu tipu: \n char - 1, int - 2, long long - 3\n");
+
+    scanf("%d", &i);
+
+   switch (i)
+   {
+        case 1:     //char
+            
+            char skaitlis1;
+            printf("ievadiet jusu skaitli: \n");
+            scanf(" %c", skaitlis1);
+            
+            
+                faktorials = skaitlis1;
+                while (skaitlis1!=2)
+                {
+                    skaitlis1--;
+                    faktorials = faktorials*skaitlis1;
+                    
+                    
+                }
+                printf("Jusu skaitla faktorials ir: %d", faktorials);
+           
+
+        break;
+
+        case 2:     //int
+
+            int skaitlis2;
+            printf("ievadiet jusu skaitli: \n");
+            scanf("%d", &skaitlis2);
+           faktorials = skaitlis2;
+                while (skaitlis2!=2)
+                {
+                    skaitlis2--;
+                    faktorials = faktorials*skaitlis2;
+                    
+                    
+                }
+                printf("Jusu skaitla faktorials ir: %d", faktorials);
+        break;
+
+        case 3:     //long long
+
+            long long int skaitlis3;
+            printf("ievadiet jusu skaitli: \n");
+            scanf("%lld", &skaitlis3);
+            
+              
+                faktorials = skaitlis3;
+                while (skaitlis3!=2)
+                {
+                    skaitlis3--;
+                    faktorials = faktorials*skaitlis3;
+                    
+                    
+                }
+                printf("Jusu skaitla faktorials ir: %d", faktorials);
+                
+            
+            
+        break;
+        default:
+        printf("pizdec");
+   }
+
+   return 0;
+
+
+
+}
+

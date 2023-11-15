151447166ae183cb8403efda0727f26c9486a21f
Aleksejs63 <aleksejs.jakimovics@gmail.com>
2023-11-15 17:30:12 +0200

    md

--- Majasdarbi/md4.c
+++ Majasdarbi/md4.c
@@ -3,76 +3,86 @@
 int main()
 {
     int i;
-    int faktorials;
+    long long int faktorials;
     printf("Izvelaties datu tipu: \n char - 1, int - 2, long long - 3\n");
 
     scanf("%d", &i);
 
-   switch (i)
-   {
+    switch (i)
+    {
         case 1:     //char
-            
+        
             char skaitlis1;
+        
             printf("ievadiet jusu skaitli: \n");
-            scanf(" %c", skaitlis1);
+            scanf("%d", &skaitlis1);
+                            
+            faktorials = skaitlis1;
+            
+            while (skaitlis1!=2)
+            {
+                skaitlis1--;
+                faktorials *= skaitlis1;
+                
+            }
+            if (faktorials>0)
+            {printf("Jusu skaitla faktorials ir: %d \n", faktorials);}
+            else 
+            {printf("you stupid \n");}
             
             
-                faktorials = skaitlis1;
-                while (skaitlis1!=2)
-                {
-                    skaitlis1--;
-                    faktorials = faktorials*skaitlis1;
-                    
-                    
-                }
-                printf("Jusu skaitla faktorials ir: %d", faktorials);
-           
-
-        break;
+            
+        
+        
+            break;
 
         case 2:     //int
-
+        
             int skaitlis2;
+
             printf("ievadiet jusu skaitli: \n");
             scanf("%d", &skaitlis2);
-           faktorials = skaitlis2;
-                while (skaitlis2!=2)
-                {
-                    skaitlis2--;
-                    faktorials = faktorials*skaitlis2;
+
+            faktorials = skaitlis2;
+            while (skaitlis2!=2)
+            {
+                skaitlis2--;
+                faktorials *= skaitlis2;
                     
-                    
-                }
-                printf("Jusu skaitla faktorials ir: %d", faktorials);
-        break;
+            }
+            if (faktorials>0)
+            {printf("Jusu skaitla faktorials ir: %d \n", faktorials);}
+            else 
+            {printf("you stupid \n");}
+
+            break;
 
         case 3:     //long long
-
+        
             long long int skaitlis3;
             printf("ievadiet jusu skaitli: \n");
             scanf("%lld", &skaitlis3);
             
-              
-                faktorials = skaitlis3;
-                while (skaitlis3!=2)
-                {
-                    skaitlis3--;
-                    faktorials = faktorials*skaitlis3;
-                    
-                    
-                }
-                printf("Jusu skaitla faktorials ir: %d", faktorials);
+        
+            faktorials = skaitlis3;
+            while (skaitlis3!=2 && faktorials>0)
+            {
+                skaitlis3--;
+                faktorials *= skaitlis3;
                 
-            
-            
-        break;
+                
+            }
+            if (faktorials>0)
+            {printf("Jusu skaitla faktorials ir: %lld \n", faktorials);}
+            else 
+            {printf("you stupid \n");}
+
+            break;
+
         default:
-        printf("pizdec");
-   }
+        printf("Ludzu ievadiet pareizu vertibu!");
+    }
 
-   return 0;
-
-
-
+    return 0;
 }
 

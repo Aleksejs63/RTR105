2bb83f61ef49cc7c1a769f8cf453654604f0c0d0
Aleksejs63 <aleksejs.jakimovics@gmail.com>
2023-12-07 18:26:30 +0200

    lab3

--- darbi/3ld_derivative/atvasinajums.c
+++ darbi/3ld_derivative/atvasinajums.c
@@ -1,14 +1,13 @@
 #undef __STRICT_ANSI__
 #include <stdio.h>
 #include <math.h>
-
-void main()
+#include <stdlib.h>
+int main()
 {
 
     FILE * pFile;
     float a, b, delta_x;
-    
-    
+    int k;
 
     printf("Cien. lietotaj ievadiet ludzu a vertibu: ");
     scanf("%f", &a);
@@ -20,44 +19,66 @@ void main()
     scanf("%f", &delta_x);
 
     pFile = fopen ("derivative.dat", "w");
-    fprintf(pFile, "\tx \n");
+    fprintf(pFile, "\tx          cos(sqrt(x))  cos'(sqrt(x)) cos''(sqrt(x)) \n");
+
 
     int i = ((b-a) / delta_x);
    
     
-    // float * x;
-    // x = (float*) malloc (i * sizeof(float)); // i * elementuskaits  japieskaita vieninieks//
-    // float * der
-//     float der1[i];
-//     float der2[i];
-    float x[i];
-    x[0] = a;
-  
-        // while(x<b)
-        // {
+    
+    float * x;
+    x = (float*) malloc ((i * sizeof(float)) + 1); 
+    if (x==NULL) exit (1);
 
-        //     fprintf(pFile, "%10.2f\t%10.2f\t%10.2f\n", x, cos(sqrt(x)), (cos(sqrt(x+delta_x))-cos(sqrt(x)))/delta_x);
-               
-        //     x += delta_x;  
+    float * func;
+    func = (float*) malloc ((i * sizeof(float)) + 1); 
+    if (func==NULL) exit (1);
 
-        // }
+    float * der1;
+    der1 = (float*) malloc ((i * sizeof(float)) + 1); 
+    if (der1==NULL) exit (1);
 
-
+    float * der2;
+    der2 = (float*) malloc ((i * sizeof(float)) + 1); 
+    if (der2==NULL) exit (1);
 
    
-    for (int k = 1; k < i+1; k++)
+  
+
+   
+    ///////////////// x masivs
+    x[0] = a;
+    for (k = 1; k < i+1; k++)
     {
-        
-        
-        x[k] =  x[k-1] + delta_x;  
-        
-        
-        
+        x[k] =  x[k-1] + delta_x;
+
     }
+    ///////////////// funkciju masivs
+    for (k = 0; k < i+1; k++)
+    {
+    
+        func[k] =  cos(sqrt(x[k]));
+
+
+    }
+    /////////////////// atvasinajums1 masivs
+    for (k = 0; k < i+1; k++)
+    {
+        der1[k] = (func[k+1] - func[k]) / delta_x;
+        if (k == i) {der1[i] = 0;}
+    }
+    //////////////////// atvadinajums2 masivs
+    for (k = 0; k < i+1; k++)
+    {
+        der2[k] = (der1[k+1] - der1[k]) / delta_x;
+        if (k == i) {der2[i] = 0;}
+        if (k == i-1) {der2[i-1] = 0;}
+    }
+
         
     for (int valdis = 0; valdis < i + 1; valdis++)
     {
-         fprintf(pFile, "%10.2f\t%10.2f\n", x[valdis], cos(sqrt(x[valdis])));       ///// funkciai jabut masiva!!
+         fprintf(pFile, "%13.4f\t%10.3e\t%10.3e\t%10.4f\n", x[valdis], func[valdis], der1[valdis], der2[valdis]);       ///// funkciai jabut masiva!!
     }
     
    
@@ -71,10 +92,14 @@ void main()
 
 /////////// izpetit malloc//////////////
 
+    free (x);
+    free (func);
+    free (der1);
+    free (der2);
 
     fclose (pFile);
     
 
-
+    return 0;
 }
 

8cf74188ee6f6da3d2569ace0a80dbc5b90d05fd
Aleksejs63 <aleksejs.jakimovics@gmail.com>
2023-12-08 18:38:10 +0200

    lab3_papildinajums

--- darbi/3ld_derivative/atvasinajums.c
+++ darbi/3ld_derivative/atvasinajums.c
@@ -19,7 +19,7 @@ int main()
     scanf("%f", &delta_x);
 
     pFile = fopen ("derivative.dat", "w");
-    fprintf(pFile, "\tx          cos(sqrt(x))  cos'(sqrt(x)) cos''(sqrt(x)) \n");
+    fprintf(pFile, "\t x       cos(sqrt(x))  analytical' cos'(sqrt(x))   analytical'' cos''(sqrt(x)) \n");
 
 
     int i = ((b-a) / delta_x);
@@ -42,8 +42,14 @@ int main()
     der2 = (float*) malloc ((i * sizeof(float)) + 1); 
     if (der2==NULL) exit (1);
 
-   
-  
+    float * der_analytical1;
+    der_analytical1 = (float*) malloc ((i * sizeof(float)) + 1); 
+    if (der_analytical1==NULL) exit (1);
+
+    float * der_analytical2;
+    der_analytical2 = (float*) malloc ((i * sizeof(float)) + 1); 
+    if (der_analytical2==NULL) exit (1);
+
 
    
     ///////////////// x masivs
@@ -74,11 +80,26 @@ int main()
         if (k == i) {der2[i] = 0;}
         if (k == i-1) {der2[i-1] = 0;}
     }
+    //////////////////// analitiska 1
+    for (k = 0; k < i+1; k++)
+    {
+    
+        der_analytical1[k] = ( (-1) * sin(sqrt(x[k])) ) / ( 2 * sqrt(x[k]) );
 
+
+    }
+    //////////////////// analitiska 2
+    for (k = 0; k < i+1; k++)
+    {
+    
+        der_analytical2[k] = ( sin(sqrt(x[k])) - sqrt(x[k]) * cos(sqrt(x[k])) ) / ( 4 * x[k] * sqrt(x[k]) );
+
+
+    }
         
     for (int valdis = 0; valdis < i + 1; valdis++)
     {
-         fprintf(pFile, "%13.4f\t%10.3e\t%10.3e\t%10.4f\n", x[valdis], func[valdis], der1[valdis], der2[valdis]);       ///// funkciai jabut masiva!!
+         fprintf(pFile, "%10.1f\t%10.1f\t%10.5f\t%10.5f\t%10.5f\t%10.5f\n", x[valdis], func[valdis], der_analytical1[valdis], der1[valdis], der_analytical2[valdis], der2[valdis]);
     }
     
    
@@ -90,12 +111,14 @@ int main()
 
 
 
-/////////// izpetit malloc//////////////
+
 
     free (x);
     free (func);
     free (der1);
     free (der2);
+    free (der_analytical1);
+    free (der_analytical2);
 
     fclose (pFile);
     

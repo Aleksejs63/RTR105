26ecb484235351bbb688f5b4d6b8b300a65a8ada
Aleksejs63 <aleksejs.jakimovics@gmail.com>
2023-12-19 18:52:59 +0200

    lab4_labojums

--- darbi/4ld_integral/integral.c
+++ darbi/4ld_integral/integral.c
@@ -13,7 +13,7 @@ int main()
 {
     float a, b, integral1 = 0., integral2, eps, h;
    
-    int n = 2, k;
+    int n = 2, k, m = 1;
 
     printf("Cien. lietotaj ludzu ievadiet a vertibu: ");
     scanf("%f", &a);
@@ -25,7 +25,7 @@ int main()
     scanf("%f", &eps);
 
     // integral2 = (b - a) * (cos(sqrt(a)) + cos(sqrt(b))) / n;
-    integral2 = (b - a) * coskv((a + b) / n);   //vieglaks pierakts, tas pats rezultats
+    integral2 = (b - a) * ( coskv(a) + coskv(b) ) / n;   //vieglaks pierakts, tas pats rezultats
     
     while (fabs(integral2 - integral1) > eps)
     {
@@ -40,15 +40,18 @@ int main()
             integral2 += h * coskv(a + (k + 0.5) * h);
         }
         
-        printf("Rectangle method integ1 = %.5f \t integ2 = %.5f \n", integral1, integral2);
+        // printf("Rectangle method integ1 = %.5f \t integ2 = %.5f \n", integral1, integral2);
 
     }
 
     printf("Integrala vertiba taisnsturu metodei: %.5f \n\n", integral2);
+
+
+
     ///////trapecu metode  
     
     n = 2;
-    integral2 = (b - a) * ( coskv(a) + coskv(b)) / n;
+    integral2 = (b - a) * ( coskv(a) + coskv(b) ) / n;
     integral1 = 0.;
     
     while (fabs(integral2 - integral1) > eps)
@@ -64,51 +67,67 @@ int main()
             integral2 += (coskv(a + (k - 1) * h) + coskv(a + (k * h))) * (h / 2);
         }
         
-        printf("Trapezoidal method integ1 = %.5f \t integ2 = %.5f \n", integral1, integral2);
+        // printf("Trapezoidal method integ1 = %.5f \t integ2 = %.5f \n", integral1, integral2);
     }
+    
     printf("Integrala vertiba trapecu metodei: %.5f \n\n", integral2);
+
+
 
     ///////Simpsona metode   NEIET
         
-    n = 2;
-    integral2 = (b - a) * ( coskv(a) + 4 * coskv((a + b) / n) + coskv(b) ) / (n * 3);
+    // n = 2;
+    // integral2 = (b - a) * ( coskv(a) + coskv(b) ) / n;
+    // integral1 = 0.;
+    // printf("Simpsona integralsis\n");
+
+    
+    
+    // while (fabs(integral2 - integral1) > eps)
+    // {
+    //     n *= 2;            // cik kopa bus attalumi h
+    //     h = (b - a) / n;    // attalums starp trapeces malam
+    //     integral1 = integral2;
+    //     integral2 = 0.;
+        
+    //     for (k = 0; k < n; k++)
+    //     {
+
+    //         integral2 += ((h) / 3.) * ( coskv(a + (k * h)) + ( (4.) * coskv(a + (k + 0.5) * h)) + coskv(a + ((k + 1) * h)) );
+
+           
+    //     }
+        
+    //     printf("Simpsons method integ1 = %.5f \t integ2 = %.5f \n", integral1, integral2);
+    // }
+
+    n = 0;
+    
+
+    integral2 = (b - a) * ( coskv(a) + coskv(b) ) / 2.;
     integral1 = 0.;
-    printf("SImpsona integralsis");
+    printf("Simpsona integralsis\n");
+
+    
     
     while (fabs(integral2 - integral1) > eps)
     {
-        n *= 2;            // cik kopa bus attalumi h
+        m *= 2;
+        n = 2 * m;            // cik kopa bus attalumi h
         h = (b - a) / n;    // attalums starp trapeces malam
-        
         integral1 = integral2;
         integral2 = 0.;
         
-        for (k = 1; k < n; k++)
+        for (k = 1; k <= m-1; k++)
         {
 
-            float nosac_a = cos(sqrt(a)), nosac_vid = cos(sqrt( ( a + (k * h)))), nosac_b = cos(sqrt(b)), nosac_vid_sum = 0.;
+            integral2 += (2.) * ( (2.) * coskv(a + ((2 * k) - 1) * h) + coskv(a + (2 * k * h)) );
 
-            // integral2 += (h / 3.0) * ( cos(sqrt(a + ( (k - 1) * h ))) + 4 * cos(sqrt(a + ( (k - 1) + 0.5 ) * h) )  + cos(sqrt(a + (k * h))) ) ;
-            // integral2 += (h / 3.0) * ( cos(sqrt(a + (h * (k - 1)))) + (4 * cos(sqrt(  ( (a + (h  * (k - 1))) + (a + (k * h)) ) / 2.  )) ) + cos(sqrt(a + (k * h))) ) ;
+            integral2 += coskv(a) + ((4.) * coskv(b-h)) + coskv(b);
 
-            // integral2 += (h / 3.0) * ( cos(sqrt(a + ( (k - 1) * h ))) + 4 * cos(sqrt((a + ( (k - 1) * h ))+h))  + cos(sqrt(a + (k * h))) );
-            for (; k < n; k++)
-            {
-                if (k % 2 == 0)
-                {
-                    nosac_vid_sum += 2*nosac_vid;
-                }
-                else
-                {
-                    nosac_vid_sum += 4*nosac_vid;
-                }
-            }
-            
-            // printf("nosac_a: %.5f\n", nosac_a );
-            // printf("nosac_vid: %.5f\n", nosac_vid );
-            // printf("nosac_b: %.5f\n", nosac_b );
-            
-            integral2 += (h / 3.0) * ( nosac_a + nosac_vid_sum + nosac_b );
+            integral2 *= (h / 3.);
+
+           
         }
         
         printf("Simpsons method integ1 = %.5f \t integ2 = %.5f \n", integral1, integral2);
